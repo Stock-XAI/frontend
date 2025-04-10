8a6e181203864b68b436da4b790d5096b40bef1e
sohyun lim <limgm7@gmail.com>
2025-04-10 17:19:07 +0900

    feat: add api basic logic

---
+++ src/api/apis.ts
@@ -0,0 +1,45 @@
+import {
+  ChartData,
+  Explanation,
+  News,
+  Prediction,
+  Stock,
+} from "../types/stock";
+import instance from "./instance";
+
+export async function getStockInfo(params: StockInfoParams) {
+  const res = await instance.get<StockInfoResponse>("stock-info", {
+    params: params,
+  });
+  return res.data;
+}
+
+export async function getSearch(params: { keyword?: string }) {
+  const res = await instance.get<SearchResponse>("search", {
+    params: params,
+  });
+  return res.data;
+}
+
+export interface StockInfoParams {
+  ticker: string;
+  horizon?: number;
+  includeNews?: boolean;
+  includeXAI?: boolean;
+}
+
+export interface StockInfoResponse {
+  success: boolean;
+  message: string;
+  data: {
+    ticker: string;
+    chartData: ChartData[];
+    news: News[];
+    prediction: Prediction;
+    explanation: Explanation;
+  };
+}
+
+export interface SearchResponse {
+  data: Stock[];
+}
---
+++ src/types/stock.ts
@@ -0,0 +1,29 @@
+export interface ChartData {
+  date: string;
+  open: number;
+  close: number;
+  high: number;
+  low: number;
+}
+
+export interface News {
+  title: string;
+  summary: string;
+  sentiment: "positive" | "negative" | "neutral";
+}
+
+export interface Prediction {
+  horizon: number;
+  result: "Rise" | "Fall";
+}
+
+export interface Explanation {
+  why: string;
+  shapValues: number[];
+  features: string[];
+}
+
+export interface Stock {
+  ticker: string;
+  name: string;
+}
