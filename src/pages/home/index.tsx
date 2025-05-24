64eba585bd7012b70eb605e8280606c071939e31
sohyun lim <limgm7@gmail.com>
2025-05-24 19:42:41 +0900

    feat: add preview contents

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"; //useRef,
+import { useEffect, useRef, useState } from "react"; //useRef,
 import { styled } from "styled-components";
 import { useStockSearch } from "../../hooks/useStockQuery";
 import { Stock } from "../../types/stock";
@@ -6,33 +6,69 @@ import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import Dropdown from "../../components/dropdown";
 
-// const sections = ["Preview", "Method", "Model", "Main Function"];
+const sections = ["Preview"]; //"Method", "Model", "Performance"
+
+const PreviewSection = () => (
+  <Wrapper>
+    <img src="/report.png" alt="Stock Prediction Chart" width="1000px" />
+    <Heading>
+      For smarter investing, <Highlight>make the most</Highlight> of our
+      service.
+    </Heading>
+    <SubText>
+      Get <Strong>predicted stock prices</Strong> for all US and Korean stocks.{" "}
+      <br />
+      See forecasts for <Strong>1-day, 7-day, and 30-day</Strong> trends.
+    </SubText>
+    <SubText>
+      We provide more than just predictions — access the{" "}
+      <Strong>latest news</Strong> <br />
+      and detailed <Strong>explanation reports</Strong> behind every forecast.
+    </SubText>
+    <Summary>
+      Based on our model’s results and reasoning, <br />
+      <Emphasis>
+        we’ll support you in making smarter investment decisions.
+      </Emphasis>
+    </Summary>
+  </Wrapper>
+);
+const MethodSection = () => <p>Here are some amazing features.</p>;
+const ModelSection = () => <p>Check out our pricing options.</p>;
+const PerformanceSection = () => <p>Frequently Asked Questions</p>;
+
+const sectionComponentMap: { [key: string]: React.ComponentType } = {
+  Preview: PreviewSection,
+  Method: MethodSection,
+  Model: ModelSection,
+  Performance: PerformanceSection,
+};
 
 function Home() {
-  // const sectionRefs = useRef<HTMLElement[]>([]);
-  // const [active, setActive] = useState("Preview");
+  const sectionRefs = useRef<HTMLElement[]>([]);
+  const [active, setActive] = useState("Preview");
   const [keyword, setKeyword] = useState("");
   const [searchResult, setSearchResult] = useState<Stock[]>([]);
   const navigate = useNavigate();
   const [horizon, setHorizon] = useState(1);
 
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver(
-  //     (entries) => {
-  //       entries.forEach((entry) => {
-  //         if (entry.isIntersecting) {
-  //           const id = entry.target.getAttribute("data-id");
-  //           if (id) setActive(id);
-  //         }
-  //       });
-  //     },
-  //     { threshold: 0.6 }
-  //   );
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const id = entry.target.getAttribute("data-id");
+            if (id) setActive(id);
+          }
+        });
+      },
+      { threshold: 0.6 }
+    );
 
-  //   sectionRefs.current.forEach((el) => el && observer.observe(el));
+    sectionRefs.current.forEach((el) => el && observer.observe(el));
 
-  //   return () => observer.disconnect();
-  // }, []);
+    return () => observer.disconnect();
+  }, []);
 
   const { data: searchData } = useStockSearch(
     { keyword },
@@ -45,10 +81,10 @@ function Home() {
     }
   }, [searchData]);
 
-  // const scrollToSection = (id: string) => {
-  //   const index = sections.findIndex((sec) => sec === id);
-  //   sectionRefs.current[index]?.scrollIntoView({ behavior: "smooth" });
-  // };
+  const scrollToSection = (id: string) => {
+    const index = sections.findIndex((sec) => sec === id);
+    sectionRefs.current[index]?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const handleStockPrediction = (item: Stock) => {
     navigate(`/detail?ticker=${item.ticker}&horizon=${horizon}`);
@@ -58,7 +94,7 @@ function Home() {
     <>
       <Navbar>
         <img src={logo} alt="logo" style={{ width: "40px", height: "auto" }} />
-        {/* {sections.map((item) => (
+        {sections.map((item) => (
           <NavItem
             key={item}
             onClick={() => scrollToSection(item)}
@@ -66,7 +102,7 @@ function Home() {
           >
             {item}
           </NavItem>
-        ))} */}
+        ))}
       </Navbar>
       <Main>
         <Title>Stock XAI.</Title>
@@ -108,21 +144,24 @@ function Home() {
         </SearchWrapper>
       </Main>
 
-      {/* <Container>
-        {sections.map((item, index) => (
-          <Section
-            key={item}
-            ref={(el: HTMLElement | null): void => {
-              if (el) sectionRefs.current[index] = el;
-            }}
-            data-id={item}
-          >
-            <h2>{item}</h2>
-            <p>This is the {item} section with some dummy text.</p>
-            <DummyContent />
-          </Section>
-        ))}
-      </Container> */}
+      <Container>
+        {sections.map((item, index) => {
+          const SectionComponent = sectionComponentMap[item] || DummyContent;
+
+          return (
+            <Section
+              key={item}
+              ref={(el: HTMLElement | null): void => {
+                if (el) sectionRefs.current[index] = el;
+              }}
+              data-id={item}
+            >
+              {/* <h2>{item}</h2> */}
+              <SectionComponent />
+            </Section>
+          );
+        })}
+      </Container>
     </>
   );
 }
@@ -183,7 +222,7 @@ const Main = styled.main`
   padding: 160px 32px;
   gap: 14px;
   background-color: ${({ theme }) => theme.systemColor.black};
-  height: 100%;
+  /* height: 100%; */
 `;
 
 const Title = styled.div`
@@ -193,41 +232,41 @@ const Title = styled.div`
   margin-top: 25px;
 `;
 
-// const NavItem = styled.div<{ $active: boolean }>`
-//   cursor: pointer;
-//   font-size: 15px;
-//   font-weight: ${({ $active }) => ($active ? "bold" : "normal")};
-//   color: ${({ $active, theme }) =>
-//     $active ? theme.systemColor.white : theme.grayColor.gray500};
-//   transition: 0.2s;
-// `;
+const NavItem = styled.div<{ $active: boolean }>`
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: ${({ $active }) => ($active ? "bold" : "normal")};
+  color: ${({ $active, theme }) =>
+    $active ? theme.systemColor.white : theme.grayColor.gray500};
+  transition: 0.2s;
+`;
 
-// const Container = styled.div`
-//   margin-top: 56px;
-// `;
+const Container = styled.div`
+  margin-top: 56px;
+`;
 
-// const Section = styled.section`
-//   height: 100vh;
-//   padding: 80px 32px 32px;
-//   background-color: ${({ theme }) => theme.systemColor.white};
-//   border-bottom: 1px solid #ccc;
+const Section = styled.section`
+  /* height: 100vh; */
+  padding: 80px 32px 32px;
+  background-color: ${({ theme }) => theme.systemColor.white};
+  /* border-bottom: 1px solid #ccc; */
 
-//   h2 {
-//     font-size: 2rem;
-//     margin-bottom: 16px;
-//   }
-// `;
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 16px;
+  }
+`;
 
-// const DummyContent = () => (
-//   <div>
-//     {[...Array(5)].map((_, i) => (
-//       <p key={i}>
-//         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at
-//         vestibulum magna.
-//       </p>
-//     ))}
-//   </div>
-// );
+const DummyContent = () => (
+  <div>
+    {[...Array(5)].map((_, i) => (
+      <p key={i}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at
+        vestibulum magna.
+      </p>
+    ))}
+  </div>
+);
 
 const SearchWrapper = styled.div`
   position: relative;
@@ -281,4 +320,50 @@ const StockInfoItem = styled.li`
   &:hover {
     background-color: ${({ theme }) => theme.grayColor.gray100};
   }
+`;
+
+const Wrapper = styled.section`
+  padding: 80px 24px;
+  width: 100%;
+  background: linear-gradient(135deg, #f5f7fa, #eef1f5);
+  text-align: center;
+  border-radius: 24px;
+  /* max-width: 800px; */
+  margin: 0 auto;
+`;
+
+const Heading = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 700;
+  margin-bottom: 24px;
+  color: #1f2d3d;
+  line-height: 1.4;
+`;
+
+const Highlight = styled.span`
+  color: #0070f3;
+`;
+
+const SubText = styled.p`
+  font-size: 1.2rem;
+  color: #4a4a4a;
+  line-height: 1.8;
+  margin-bottom: 24px;
+`;
+
+const Strong = styled.span`
+  font-weight: bold;
+  color: #222;
+`;
+
+const Summary = styled.div`
+  margin-top: 32px;
+  font-size: 1.15rem;
+  line-height: 1.8;
+  color: #2a2a2a;
+`;
+
+const Emphasis = styled.span`
+  font-weight: 600;
+  color: #0070f3;
 `;
